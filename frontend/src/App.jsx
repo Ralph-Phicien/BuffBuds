6763c7de94cb35108c2b1b96b86e1b244dcb9293
DylanLiesenfelt <120532193+DylanLiesenfelt@users.noreply.github.com>
2025-09-26 11:59:13 -0400

    build: Init page/routes
    Initial React layout for sign-in and up pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,20 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom' 
+import SignIn from './assets/pages/SignIn'
+import Signup from './assets/pages/signup'
+import Feed from './assets/pages/feed'
+
 
 function App() {
 
   return (
     <>
       <div>
-       BuffBuds
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
       </div>
     </>
   )
