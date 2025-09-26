100c5f3dd37ff729da7345d176eb7365c101e80a
DylanLiesenfelt <120532193+DylanLiesenfelt@users.noreply.github.com>
2025-09-26 12:16:43 -0400

    build: basic form

--- frontend/src/assets/pages/SignIn.jsx
+++ frontend/src/assets/pages/SignIn.jsx
@@ -1,7 +1,13 @@
 const Signin = () => {
     return (
         <div>
-            <h1>Sign In Page</h1>
+            <h1>Welcome to BuffBuds!</h1>
+            <form action="">
+                <input type="email" placeholder="Email" />
+                <input type="password" placeholder="Password" />
+                <button type="submit">Sign In</button>
+                <p>Don't have an account? <a href="/signup">Sign Up</a></p>
+            </form>
         </div>
     )
 }
--- frontend/src/assets/pages/SignUp.jsx
+++ frontend/src/assets/pages/SignUp.jsx
@@ -1,7 +1,14 @@
 const Signup = () => {
     return (
         <div>
-            <h1>Signup Page</h1>
+            <form action="">
+                <h1>Join BuffBuds Today!</h1>
+                <input type="text" placeholder="Username" />
+                <input type="email" placeholder="Email" />
+                <input type="password" placeholder="Password" />
+                <button type="submit">Sign Up</button>
+                <p>Already have an account? <a href="/signin">Sign In</a></p>
+            </form>
         </div>
     )
 }
